421f1940be570a3cad4225a8856edea79114bc89
Vitaliy <vortaxg@gmail.com>
2017-09-03 22:55:44 +0300

    add recipe select behaviour

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import { Recipe } from '../../recipe.model';
 @Component({
   selector: 'app-recipe-item',
@@ -7,8 +13,11 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Output() selectedRecipe = new EventEmitter<void>();
   constructor() { }
   ngOnInit() {
   }
-
+  onSelected() {
+    this.selectedRecipe.emit();
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter
+} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +12,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test Recipe Name',
-               'The Recipe Description',
-               'http://maxpixel.freegreatpicture.com/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg'),
+      'The Recipe Description',
+      'http://maxpixel.freegreatpicture.com/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg'),
     new Recipe('Struffoli Meal Food Sweet',
-               'The Recipe Description',
-               'http://maxpixel.freegreatpicture.com/static/photo/1x/Struffoli-Meal-Food-Sweet-Typical-Sweet-Recipe-647314.jpg')];
+      'The Recipe Description2',
+      'http://maxpixel.freegreatpicture.com/static/photo/1x/Struffoli-Meal-Food-Sweet-Typical-Sweet-Recipe-647314.jpg')];
   constructor() { }
   ngOnInit() {
+  }
+  onSelectedRecipe(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
